241f241d4c22dca6b631cf5f0ecf6aa8b759aee1
Eric Powell <rhazzed@gmail.com>
2017-10-07 04:19:34 -0500

    Took a series of pictures in take_picture.py

--- take_picture.py
+++ take_picture.py
@@ -16,22 +16,28 @@ from time import sleep
 
 
 
-def analyze_image(image_data):
+def analyze_image(image_data, filename):
     '''Process image'''
-    newfile = open('test.jpeg', 'wb')
+    #newfile = open('test.jpeg', 'wb')
+    newfile = open(filename, 'wb')
     newfile.write(image_data)
     newfile.close()
     return "Ok"
 
-def do_photo(robot: cozmo.robot.Robot):
+def do_photos(robot: cozmo.robot.Robot):
     '''Get current image from cozmo camera'''
-    print("Waiting for a picture...")
+    print("Setting up camera for color pictures...")
     robot.camera.color_image_enabled = True
     robot.camera.image_stream_enabled = True
     sleep(2) # Give camera time to switch to color
+    ##for x in range(1, 101):
+    for x in range(1, 31):
+        do_photo(robot, 'test.%d.jpeg' % (x))
 
 
+def do_photo(robot: cozmo.robot.Robot, filename):
     # wait for a new camera image to ensure it is captured properly
+    print("Waiting for a picture...")
     robot.world.wait_for(cozmo.world.EvtNewCameraImage) # <<< script crawls HERE
     print("Found a picture, capturing the picture.")
 
@@ -45,18 +51,17 @@ def do_photo(robot: cozmo.robot.Robot):
         # reset file pointer to start
         in_mem_file.seek(0)
         img_bytes = in_mem_file.read()
-        #image_64_encode = base64.b64encode(img_bytes)
-        #image_data = 'data:image/jpeg;base64,'+image_64_encode.decode("utf-8")
-        #analyze_image(image_data)
-        analyze_image(img_bytes)
+        analyze_image(img_bytes, filename)
+
         cozmo.logger.info("Success")
         # return image_data
     else:
         cozmo.logger.info("Error")
-        return "Error: I have no photos"
+        print("Error: I have no photo")
+
 
 def do_say(robot: cozmo.robot.Robot):
     robot.say_text("Hello World").wait_for_completed()
 
 cozmo.robot.Robot.drive_off_charger_on_connect = False
-cozmo.run_program(do_photo)
+cozmo.run_program(do_photos)
